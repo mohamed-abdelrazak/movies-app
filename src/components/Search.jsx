f621134300b090a524707e314df2cca2e3e2b036
mohamed-abdelrazak <zoka1080p@gmail.com>
2025-03-14 23:15:00 +0200

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Search({ searchTerm, setSearchTerm }) {
+  return (
+    <div className="search">
+      <div>
+        <img src="search.svg" alt="search" />
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+      </div>
+    </div>
+  );
+}
